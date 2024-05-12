59ca292a713c9cd93ba30a5b30d281bb143ef5a7
Son Hai Nguyen <sirarseus@gmail.com>
2024-05-12 14:13:32 +0200

    add: sr manipulation functions

--- lib/peripheral/shift_register.c
+++ lib/peripheral/shift_register.c
@@ -1,6 +1,7 @@
 #include "shift_register.h"
 #include <stdlib.h>
 #include <gpioex.h>
+#include <delay.h>
 
 void sr_init(ShiftRegister *sr, PortLinkedPin *clk_pin, PortLinkedPin *data_pin, PortLinkedPin *cs_pin)
 {
@@ -14,4 +15,33 @@ void sr_init(ShiftRegister *sr, PortLinkedPin *clk_pin, PortLinkedPin *data_pin,
     GPIOex_Init(clk_pin, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIOex_Init(data_pin, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIOex_Init(cs_pin, GPIO_MODE_OUT_PP_HIGH_SLOW);
+}
+
+void sr_set_data(ShiftRegister *sr, uint8_t data)
+{
+    if (sr == NULL)
+        return;
+
+    uint8_t bit_mask;
+    uint8_t i = 0;
+
+    for (bit_mask = 0b10000000; bit_mask; bit_mask >>= 1)
+    {
+        GPIOex_Write(sr->data_pin, bit_mask & data); // sent bit to the SR
+        for (i = 0; i < 2; i++)                         // send clock pulse
+        {
+            GPIOex_WriteReverse(sr->clk_pin); // the SR will react on rising edge of clock
+            _delay_us(1);
+        }
+    }
+}
+
+void sr_confirm(ShiftRegister *sr)
+{
+    if (sr == NULL)
+        return;
+
+    GPIOex_WriteHigh(sr->cs_pin);
+    _delay_us(1);
+    GPIOex_WriteLow(sr->cs_pin);
 }
--- lib/peripheral/shift_register.h
+++ lib/peripheral/shift_register.h
@@ -2,6 +2,7 @@
 #define _SHIFT_REGISTER_H_
 
 #include <port_linked_pin.h>
+#include <stdint.h>
 
 typedef struct
 {
@@ -11,5 +12,15 @@ typedef struct
 } ShiftRegister;
 
 void sr_init(ShiftRegister *sr, PortLinkedPin *clk_pin, PortLinkedPin *data_pin, PortLinkedPin *cs_pin);
+
+/**
+ * @brief sr_set_Data Set data to a shift register. However, the data is not set to the output, it needs to be confirmed.
+ */
+void sr_set_data(ShiftRegister *sr, uint8_t data);
+
+/**
+ * @brief sr_confirm Data that has been sent to the shift register will be set to output pins of the register after confirmation.
+ */
+void sr_confirm(ShiftRegister *sr);
 
 #endif
