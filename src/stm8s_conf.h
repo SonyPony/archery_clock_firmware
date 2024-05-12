7bd72fe89294d2c7d34e8d2278452925df6c7bbb
Son Hai Nguyen <sirarseus@gmail.com>
2024-05-12 11:34:42 +0200

    add: uart3 dependency

--- src/stm8s_conf.h
+++ src/stm8s_conf.h
@@ -83,7 +83,7 @@
 #endif /* (STM8S105) || (STM8AF626x) */
 #if defined(STM8S208) || defined(STM8S207) || defined(STM8S007) || defined(STM8AF52Ax) || \
     defined(STM8AF62Ax)
-// #include "stm8s_uart3.h"
+ #include "stm8s_uart3.h"
 #endif                  /* (STM8S208) || (STM8S207) || (STM8AF52Ax) || (STM8AF62Ax) */
 #if defined(STM8AF622x) /* SDCC patch: see https://github.com/tenbaht/sduino/tree/master/STM8S_StdPeriph_Driver */
 // #include "stm8s_uart4.h"
