2db037324cd17f788de5ef0583437669f8ae6f12
Son Hai Nguyen <sirarseus@gmail.com>
2024-07-13 10:15:40 +0200

    refactor: extend logging to logging levels

--- lib/logging/logging.cpp
+++ lib/logging/logging.cpp
@@ -4,6 +4,35 @@
 #include <string.h>
 #include <pico/stdio.h>
 
+void Logging::log(LoggingLevel logLevel, const char *format, ...)
+{
+    switch (logLevel)
+    {
+    case LoggingLevel::Debug:
+        printf("DEBUG: ");
+        break;
+
+    case LoggingLevel::Warning:
+        printf("WARNING: ");
+        break;
+
+    case LoggingLevel::Info:
+        printf("INFO: ");
+        break;
+
+    case LoggingLevel::Error:
+        printf("ERROR: ");
+        break;
+
+    default:
+        break;
+    }
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+
 void Logging::log(const char *format, ...)
 {
     va_list args;
--- lib/logging/logging.h
+++ lib/logging/logging.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
+enum LoggingLevel {
+    Info,
+    Debug,
+    Warning,
+    Error,
+};
+
 namespace Logging {
     void log(const char *format, ...);
+    void log(LoggingLevel logLevel, const char *format, ...);
 }
 
 #endif
