c7123df2bc9112faafcf148fe45e05abe46416d9
Son Hai Nguyen <sirarseus@gmail.com>
2025-04-20 11:00:05 +0200

    feat: add async beeping

--- lib/peripheral/beeper.cpp
+++ lib/peripheral/beeper.cpp
@@ -1,17 +1,61 @@
 #include "beeper.h"
 #include "pico/stdlib.h"
+#include <lib/logging/logging.h>
 
-Beeper::Beeper(uint32_t beeperPin): m_beeperPin(beeperPin, GPIOMode::GPIOOut, false) 
+Beeper::Beeper(uint32_t beeperPin): m_beeperPWM(beeperPin), m_currentBeepCounter(0)
 {}
 
 void Beeper::beep(uint8_t beepCount) {
-    for(uint8_t i = 0; i < beepCount; i++) {
-        for(int j = 0; j < 250; j++) {  // 250 PWM pulses, TODO parametrize to duration
-            this->m_beeperPin.writeHigh();
-            sleep_us(63);
-            this->m_beeperPin.writeLow();
-            sleep_us(63);
-        }
-        sleep_ms(100);
-    }
+    this->m_currentBeepCounter = beepCount;
+    
+    this->singleBeep();     // it will automatically follow up with other beeps
+}
+
+void Beeper::singleBeep() {
+    if(this->m_currentBeepCounter <= 0) 
+        return;
+
+    Logging::log(LoggingLevel::Debug, "Start beep\n");
+
+    this->m_beeperPWM.setParameters(this->m_signalFrequency, 50);
+    this->m_beeperPWM.start();
+    this->m_currentBeepCounter--;
+
+    // add callback to stop beep and possibly start pause between beeps
+    add_alarm_in_ms(        
+        this->m_beepDuration, 
+        Beeper::handleSingleBeepEnd,
+        this, 
+        false
+    );
+}
+
+int64_t Beeper::handleSingleBeepEnd(alarm_id_t alaramId, void* data) {
+    Beeper* self = static_cast<Beeper*>(data);
+    self->m_beeperPWM.stop();
+    Logging::log(LoggingLevel::Debug, "End beep.\n");
+
+    if(self->m_currentBeepCounter <= 0)   // no more beeps, end it
+        return false;
+
+    // start pause between beeps
+    add_alarm_in_ms(
+        self->m_betweenBeepsPauseDuration,
+        Beeper::handleSingleBeepStart,
+        self,
+        false
+    );
+
+    return false;
+}
+
+int64_t Beeper::handleSingleBeepStart(alarm_id_t alaramId, void* data) {
+    Beeper* self = static_cast<Beeper*>(data);
+
+    if(self->m_currentBeepCounter <= 0)   // no more beeps, end it
+        return false;
+
+    self->singleBeep();
+
+    return false;
 }
--- lib/peripheral/beeper.h
+++ lib/peripheral/beeper.h
@@ -3,17 +3,29 @@
 
 #include <stdint.h>
 #include <lib/io/gpioex.h>
+#include <lib/io/pwmex.h>
+#include <hardware/clocks.h>
 
 class Beeper
 {
     private:
-        GPIOPin m_beeperPin;
+        PWMPin m_beeperPWM;
+        uint32_t m_signalFrequency;               // in Hz for PWM
+        uint32_t m_beepDuration;                  // in ms
+        uint32_t m_betweenBeepsPauseDuration;     // in ms
+        uint8_t m_currentBeepCounter;
+
+    protected:
+        static int64_t handleSingleBeepEnd(alarm_id_t alarmId, void* data);
+        static int64_t handleSingleBeepStart(alarm_id_t alarmId, void* data);
+
+        void singleBeep();
+
 
     public:
-        Beeper(uint32_t beeperPin);
+        Beeper(uint32_t beeperPin, uint32_t sinalFrequency, uint32_t beepDuration, uint32_t beepsPauseDuration);
         
         void beep(uint8_t beepCount);
-
 };
 
 #endif
